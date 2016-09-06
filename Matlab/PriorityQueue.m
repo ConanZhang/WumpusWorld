43fee1d2fdcb3e7adee3e598231e4f0c2c4298d3
rajulr <rajulr@eng.utah.edu>
2016-09-05 20:18:20 -0600

    added cellarray implementation

--- Matlab/PriorityQueue.m
+++ Matlab/PriorityQueue.m
@@ -3,20 +3,20 @@ classdef PriorityQueue
     % A simple queue that sorts the priority ascending or descending
     
     properties
-        queue = []
+        queue = {};
     end
     
     methods
         function obj = insert(obj, node, insertBefore)
             if isempty(obj.queue) == 1
-               obj.queue(1, 1) = node(1,5); 
+               obj.queue{1,1} = node; 
                return
             end
             
             if insertBefore == 1
                 success = 0;
                 for i = 1:length(obj.queue)
-                    if node(1,5) <= obj.queue(1, i)
+                    if node(1,5) <= obj.queue{1, i}(1,5)
                         obj.queue = [obj.queue(:,1:i-1) node obj.queue(:,i:end)];
                         success = 1;
                         break
@@ -24,20 +24,20 @@ classdef PriorityQueue
                 end
                 
                 if success == 0
-                    obj.queue(1, end+1) = node;
+                    obj.queue{end+1} = node;
                 end
             else
                 for i = 1:length(obj.queue)
                     success = 0;
-                    if node(1,5) <= obj.queue(1, i)
+                    if node(1,5) <= obj.queue{1, i}(1,5)
                         obj.queue = [obj.queue(:,1:i) node obj.queue(:,i+1:end)];
                         success = 1;
                         break
                     end
                 end
-                
+                %TODO: Check to see what insert after before or equal means
                 if success == 0
-                    obj.queue(1, end+1) = node;
+                    obj.queue{end+1} = node;
                 end
             end   
         end
