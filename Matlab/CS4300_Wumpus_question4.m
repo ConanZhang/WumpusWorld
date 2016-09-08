728ef770a325b6d63b12ec2c12f0daa5aa2ccbcb
rajulr <rajulr@eng.utah.edu>
2016-09-08 04:22:53 -0600

    q4 change

--- Matlab/CS4300_Wumpus_question4.m
+++ Matlab/CS4300_Wumpus_question4.m
@@ -4,7 +4,7 @@ function no_of_nodes = CS4300_Wumpus_question4(state, action, board)
     for i = 1:4
         for j = 1:4
            board(5-i,j) = 2;
-           solution = CS4300_Wumpus_A_star1(board,[1,1,0],[i,j,1],'CS4300_A_star_Man',1);
+           solution = CS4300_Wumpus_A_star1(board,[1,1,0],[i,j,1],'CS4300_A_star_Man',2);
            
            s = size(solution);
            sizex= s(1);
